e0fa442b9cdc44f1b4b57582fb4d8bd82a02c946
futurbyte-muhammadhassan <muhammad.hassan@futurbyte.ae>
2024-08-27 16:16:12 +0500

    feat: AppConstants, Private Constructor to Prevent Instantiation

---
+++ lib/utilities/app_constants.dart
@@ -0,0 +1,8 @@
+class AppConstants{
+  // Private Constructor to Prevent Instantiation
+  AppConstants._();
+  // Static Constant Fields
+  static const double PADDING = 15;
+  static const String STARTDATE = "Start Date";
+  static const String API_KEY = "AIzaSyAVJiRDZsecoVecl0fiq2xHchQdWihXr3k";
+}
